5566e125f39fe69ebd58b2768564b6fe6c6be4af
Mehmet Yarar <niyowski@gmail.com>
2023-12-20 12:52:12 +0600

    feat(presentation): add linkedin link to author slide

--- apps/presentation/src/slides/author.tsx
+++ apps/presentation/src/slides/author.tsx
@@ -5,20 +5,36 @@ import {
   BriefcaseIcon,
   CodeIcon,
   GlobeIcon,
+  LinkedinIcon,
   ShieldCheckIcon,
 } from 'lucide-react';
 
+import { Anchor } from '~/components/anchor';
 import { BulletedList, type BulletPoint } from '~/components/bulleted-list';
 import { Image } from '~/components/image';
 import { Slide } from '~/reveal/templates/slide';
 
-const name = 'Mehmet Yarar';
+const authorName = 'Mehmet Yarar';
 
 const contact: BulletPoint[] = [
   {
     id: '1',
     icon: { component: AtSignIcon },
     text: { content: 'niyowski' },
+  },
+  {
+    id: '2',
+    icon: { component: LinkedinIcon },
+    text: {
+      content: (
+        <Anchor
+          href="https://linkedin.com/in/mnyarar"
+          aria-label={`LinkedIn page of ${authorName}`}
+        >
+          mnyarar
+        </Anchor>
+      ),
+    },
   },
 ];
 
@@ -62,13 +78,13 @@ const highlights: BulletPoint[] = [
 
 export default () => {
   return (
-    <Slide id="author" title={name} alignTop>
+    <Slide id="author" title={authorName} alignTop>
       <div className="flex">
         <BulletedList items={highlights} className="flex-1" />
         <div className="flex flex-col items-center">
           <Image
             src="/images/author.png"
-            alt={name}
+            alt={authorName}
             className="m-0 w-60 rounded-full shadow-2xl hue-rotate-90"
           />
           <BulletedList items={contact} />
